6cd8e436c035b9410e8277f1480b60d2766f7fd9
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-05 22:23:46 -0500

    orphan pages

--- OrphanPages.java
+++ OrphanPages.java
@@ -18,45 +18,68 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class OrphanPages extends Configured implements Tool {
-    public static final Log LOG = LogFactory.getLog(OrphanPages.class);
+  public static final Log LOG = LogFactory.getLog(OrphanPages.class);
 
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new OrphanPages(), args);
-        System.exit(res);
-    }
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Configuration(), new OrphanPages(), args);
+    System.exit(res);
+  }
+
+  @Override
+  public int run(String[] args) throws Exception {
+    Job job = Job.getInstance(this.getConf(), "Orphan Pages");
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(NullWritable.class);
+
+    job.setMapOutputKeyClass(IntWritable.class);
+    job.setMapOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(LinkCountMap.class);
+    job.setReducerClass(OrphanPageReduce.class);
+
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.setJarByClass(OrphanPages.class);
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
+    private static final IntWritable ZERO = new IntWritable(0);
+    private static final IntWritable ONE = new IntWritable(1);
+    private IntWritable link = new IntWritable(0);
 
     @Override
-    public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(this.getConf(), "Orphan Pages");
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(NullWritable.class);
+    public void map(Object key, Text value, Context context)
+        throws IOException, InterruptedException {
+      StringTokenizer stk = new StringTokenizer(value.toString(), ": ");
 
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(IntWritable.class);
-
-        job.setMapperClass(LinkCountMap.class);
-        job.setReducerClass(OrphanPageReduce.class);
-
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-        job.setJarByClass(OrphanPages.class);
-        return job.waitForCompletion(true) ? 0 : 1;
+      //
+      // Let's assume parent is an orphan page. If it is linked, there will be another record written while
+      // processing that parent.
+      //
+      link.set(Integer.parseInt(stk.nextToken()));
+      context.write(link, ZERO);
+      while (stk.hasMoreTokens()) {
+        link.set(Integer.parseInt(stk.nextToken()));
+        context.write(link, ONE);
+      }
     }
+  }
 
-    public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
-        @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            //TODO
-            //context.write(<IntWritable>, <IntWritable>); // pass this output to reducer
-        }
+  public static class OrphanPageReduce
+      extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
+    @Override
+    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
+      int count = 0;
+      for (IntWritable e : values) {
+        count += e.get();
+        if (count > 0) break;
+      }
+      if (count == 0) {
+        context.write(key, NullWritable.get());
+      }
     }
-
-    public static class OrphanPageReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
-        @Override
-        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //TODO
-            //context.write(<IntWritable>, <NullWritable>); // print as final output
-        }
-    }
+  }
 }
